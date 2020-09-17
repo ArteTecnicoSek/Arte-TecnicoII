3bdb18f0e9005cff2edca7fe31366041906e47f2
andres10 <andres@piola.dev>
2020-09-17 16:26:23 -0300

    first push

---
+++ SesionArteTecnicoII/Assets/TimelineTrigger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class TimelineTrigger : MonoBehaviour
+{
+    public int index;
+    public TimelineAsset timeline;
+    public PlayableDirector controlPlayable;
+
+    public static event FNotify_1Params<int> OnPlayTimeline;
+    public static event FNotify OnTimelineEnd;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnDisable()
+    {
+        OnTimelineEnd?.Invoke();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+           controlPlayable.Play();
+            OnPlayTimeline?.Invoke(index);
+
+
+        }
+    }
+}
